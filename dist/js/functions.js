de58c5cce6a5f231197b2ea0c6c809d5696aa0f0
Vitalik885 <Vitalik885@gmail.com>
2019-12-29 23:46:02 +0200

    creating imgs

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -22,9 +22,20 @@ navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
 
 snap.addEventListener('click', function () {
     context.drawImage(video, 0, 0, 640, 480);
-    var base64dataUrl = canvas.toDataURL('image/png');
+    var base64dataUrl = canvas.toDataURL('image/jpeg');
+
     context.setTransform(1, 0, 0, 1, 0, 0);
     var img = new Image();
     img.src = base64dataUrl;
+    var new_img = document.createElement('img');
+    var new_href = document.createElement('a');
+    new_href.className = "href__photo";
+    new_href.setAttribute('download','download');
+    new_href.href = img.src;
+    new_img.className = "snap__photo";
+    new_img.src = img.src;
+    document.querySelector('.images__container').appendChild(new_href);
+    document.querySelector('.href__photo').appendChild(new_img);
+
     console.log(img);
 });
