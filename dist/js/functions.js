e1f2c394de631748a3dd4e7ba93924106f22ae99
Vitalik885 <Vitalik885@gmail.com>
2019-12-29 22:39:28 +0200

    addded canvas

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,18 +1,30 @@
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var snap = document.getElementById('snap');
 
-    video = document.getElementById('vid');
-    video.style.width = document.width + 'px';
-    video.style.height = document.height + 'px';
-    video.setAttribute('autoplay', '');
-    video.setAttribute('muted', '');
-    video.setAttribute('playsinline', '');
+video.style.width = document.width + 'px';
+video.style.height = document.height + 'px';
+video.setAttribute('autoplay', '');
+video.setAttribute('muted', '');
+video.setAttribute('playsinline', '');
 
-    var constraints = {
-        audio: false,
-        video: {
-            facingMode: 'user'
-        }
+var constraints = {
+    audio: false,
+    video: {
+        facingMode: 'user'
     }
+}
 
-    navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-        video.srcObject = stream;
-    });
+navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+    video.srcObject = stream;
+});
+
+snap.addEventListener('click', function () {
+    context.drawImage(video, 0, 0, 640, 480);
+    var base64dataUrl = canvas.toDataURL('image/png');
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    var img = new Image();
+    img.src = base64dataUrl;
+    console.log(img);
+});
